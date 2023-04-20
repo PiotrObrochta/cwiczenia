34fb2b1f2913daab05357e760d1479d91fe105c0
PiotrObrochta <piotr.obrochta88@gmail.com>
2023-04-20 10:38:18 +0200

    add exercises 8.

--- js/script.js
+++ js/script.js
@@ -9,46 +9,50 @@ const mergeArrays = [
 
 console.log(mergeArrays);
 
-// -----------------------------------------------------------
+// ------------------------------------------------------------------
 
 // ćwiczenie 2.
 
 const names = [2, "delfin", null];
+
 const exceptFirst = (names) => names.slice(1);
 
 console.log(exceptFirst(names));
 
-// -----------------------------------------------------------
+// ------------------------------------------------------------------
 
 // ćwiczenie 3.
 
 const parameters = [1, 3, undefined, true];
 
-const last2Parameters = (parameters) => parameters.slice(2)
-console.log(last2Parameters(parameters))
+const last2Parameters = (parameters) => parameters.slice(2);
 
-// -----------------------------------------------------------
+console.log(last2Parameters(parameters));
+
+// ------------------------------------------------------------------
 
 // ćwiczenie 4.
 
 const kawa = ["rum", "kawa", "sangria"];
-const indexOf = () => kawa.indexOf("kawa")
+
+const indexOf = () => kawa.indexOf("kawa");
+
 console.log(indexOf(kawa));
 
-// -----------------------------------------------------------
+// ------------------------------------------------------------------
 
 // ćwiczenie 5.
 
 const tasks = [
-    {content: ""},
-    {content: "kupić balony w kształcie psów"}, 
+    { content: "" },
+    { content: "kupić balony w kształcie psów" },
 ]
 
-const findNonEmptyTask = (tasks) => tasks.find(({content}) => content !== "");
+const findNonEmptyTask = (tasks) => tasks.find(({ content }) => content !== "");
 
 console.log(findNonEmptyTask(tasks));
 
-// -----------------------------------------------------------
+// ------------------------------------------------------------------
 
 // ćwiczenie 6.
 
@@ -58,10 +62,27 @@ const oddIndex = numbers.findIndex(number => number % 2 !== 0);
 
 console.log(oddIndex);
 
-// -----------------------------------------------------
+// ------------------------------------------------------------------
 
-// ćwiczenie 7
+// ćwiczenie 7.
 
 const fruits = ["banan", "marakuja"];
+
 const hasStrawberry = (fruits) => fruits.includes("truskawka");
+
 console.log(hasStrawberry(fruits));
+
+// ------------------------------------------------------------------
+
+// ćwiczenie 8.
+
+const people = ([
+    { name: "Melodia", age: 15 },
+    { name: "Kosmo", age: 18 },
+]);
+
+const someAdult = (people) => people.some(({ age }) => age >= 18);
+
+console.log(someAdult(people));
+
+// ------------------------------------------------------------------
